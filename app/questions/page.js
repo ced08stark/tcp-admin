fa74ce6a832481490bbcd43d5e77b61ee1d75c08
ced_stark08 <cedricngangue08@gmail.com>
2023-08-07 23:38:11 +0200

    four commit

--- app/questions/page.js
+++ app/questions/page.js
@@ -1,8 +1,7 @@
 "use client";
-import QuestionsRows from "@/components/questionsRows";
+import QuestionsRows from "../../components/QuestionsRows";
 import React from "react";
 import Image from "next/image";
-import * as Icons from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 
 function QuestionsPage() {
@@ -24,55 +23,7 @@ function QuestionsPage() {
       >
         Add question
       </button>
-      <div
-        data-te-modal-init
-        className="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
-        id="exampleModalXl"
-        tabindex="-1"
-        aria-labelledby="exampleModalXlLabel"
-        aria-modal="true"
-        role="dialog"
-      >
-        <div
-          data-te-modal-dialog-ref
-          className="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px] min-[992px]:max-w-[800px] min-[1200px]:max-w-[1140px]"
-        >
-          <div className="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-            <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-              <h5
-                className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
-                id="exampleModalXlLabel"
-              >
-                Extra large modal
-              </h5>
-
-              <button
-                type="button"
-                className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-                data-te-modal-dismiss
-                aria-label="Close"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            <div className="relative p-4">...</div>
-          </div>
-        </div>
-      </div>
+    
       {/* <div className="w-full flex items-center">
         <table className="w-[80%]  border">
           <thead className="bg-gray-50">
