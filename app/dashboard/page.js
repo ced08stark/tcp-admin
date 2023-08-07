5d052a35a1f2d92819a98eafd693bf24f77ec52a
ced_stark08 <cedricngangue08@gmail.com>
2023-08-08 00:30:14 +0200

    ten commit

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -33,7 +33,7 @@ export default function Home() {
           </div>
         </Link>
         <Link
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/"
           className="group rounded-lg overflow-hidden border relative hover:bg-white border-1 m-2 px-5 py-4 transition-colors hover:border-gray-300 bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30 flex justify-between"
           target="_blank"
           rel="noopener noreferrer"
@@ -62,7 +62,7 @@ export default function Home() {
         </Link>
 
         <Link
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/"
           className="group rounded-lg relative overflow-hidden hover:bg-white border m-2 px-5 py-4 transition-colors hover:border-gray-300 bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-between"
           target="_blank"
           rel="noopener noreferrer"
@@ -91,7 +91,7 @@ export default function Home() {
         </Link>
 
         <Link
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/"
           className="group rounded-lg relative overflow-hidden hover:bg-white border border-1 m-2 px-5 py-4 transition-colors hover:border-gray-300 bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-between"
           target="_blank"
           rel="noopener noreferrer"
