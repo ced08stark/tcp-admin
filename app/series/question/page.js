09f6184dece5b700f4d09c6951f1e1580d7ae75d
ced_stark08 <cedricngangue08@gmail.com>
2023-08-18 12:56:10 +0200

    update question okay

--- app/series/question/page.js
+++ app/series/question/page.js
@@ -450,7 +450,7 @@ function QuestionsPage() {
                   <div className="w-full h-[100px] m-3 justify-center flex">
                     <Image
                       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
-                      src={` ${image ? image : currentQuestion?.libelle}`}
+                      src={`${image ? image : currentQuestion?.libelle}`}
                       alt="Next.js Logo"
                       width={180}
                       height={37}
